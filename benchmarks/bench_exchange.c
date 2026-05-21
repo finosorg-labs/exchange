e6e1184614291f6ca18dbfbe11ce7e934848535b
FINOS LABS <app.echietech@gmail.com>
2026-05-21 17:36:02 +0800

    add ticker

--- benchmarks/bench_exchange.c
+++ benchmarks/bench_exchange.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 /* External benchmark functions from sub-modules */
-extern void bench_xxx_run(void);
+extern void bench_ticker_run(void);
 
 /* Entry point for exchange benchmarks */
 void bench_exchange_run(void) {
@@ -22,7 +22,7 @@ void bench_exchange_run(void) {
     printf("============================================================\n");
 
     /* Run all sub-module benchmarks */
-    bench_xxx_run();
+    bench_ticker_run();
 
     printf("\n");
     printf("============================================================\n");
